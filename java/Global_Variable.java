d244d7a823c352814da8a4be823105430b5b21ff
0607mathi <mathiyarasu.m0607@gmail.com>
2024-10-17 22:30:28 +0530

    java program solved

---
+++ java/Global_Variable.java
@@ -0,0 +1,12 @@
+class Global_Variable{
+    static String Name;
+    static int Employee_Id;
+    static long Contact;
+    static float Salary;
+    public static void main(String args[]){
+        System.out.println(Name);
+        System.out.println(Employee_Id);
+        System.out.println(Contact);
+        System.out.println(Salary);
+    }
+}
